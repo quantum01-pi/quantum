44ee36fb51b50477292b0ec1264d8e8228d61671
msiuri <48696872+msiuri@users.noreply.github.com>
2020-04-26 22:01:30 -0300

    CRUD PRODUTO V1
    upgrade ainda n funcinal

--- backend/main.py
+++ backend/main.py
@@ -56,6 +56,80 @@ def not_found(error=None):
     respone.status_code = 404
     return respone
 
+@app.route('/produto/adicionar', methods=['POST'])
+def add_produto():
+	try:
+		_json = request.json
+		_nome_produto = _json['nome_produto']
+		_quantidade = _json['quantidade']
+		_tipo_qtd = _json['tipo_qtd']
+		_validade = _json['validade']
+		_usuario_id_usuario = _json['usuario_id_usuario']
+		if _nome_produto and _quantidade and _tipo_qtd and _usuario_id_usuario and _validade and request.method == 'POST':			
+			sqlQuery = "INSERT INTO produto(nome_produto, quantidade, tipo_qtd, usuario_id_usuario, validade) VALUES(%s, %s, %s, %s, %s)"
+			bindData = (_nome_produto, _quantidade, _tipo_qtd, _usuario_id_usuario, _validade)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sqlQuery, bindData)
+			conn.commit()
+			_result = jsonify({'description': 'Cadastrado com sucesso'})
+			response = _result
+			response.status_code = 200
+			return response
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/produto', methods=['GET'])
+def lista_produto():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT * FROM produto")
+		empRows = cursor.fetchall()
+		respone = jsonify(empRows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/produto/atualizar', methods=['POST'])
+def atualizar_produto():
+	try:
+		_json = request.json
+		_id_produto = _json['id_produto']
+		_nome_produto = _json['nome_produto']
+		_quantidade = _json['quantidade']
+		_tipo_qtd = _json['tipo_qtd']
+		_validade = _json['validade']
+		_usuario_id_usuario = _json['usuario_id_usuario']
+
+		if _id_produto and _nome_produto and _quantidade and _tipo_qtd and _usuario_id_usuario and _validade and request.method == 'POST':			
+			sqlQuery = "UPDATE produto WHERE id_produto '%s' (nome_produto, quantidade, tipo_qtd, usuario_id_usuario, validade) VALUES(%s, %s, %s, %s, %s)"
+			bindData = (_nome_produto, _quantidade, _tipo_qtd, _usuario_id_usuario, _validade)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sqlQuery, bindData)
+			conn.commit()
+			_result = jsonify({'description': 'Atualizado com sucesso'})
+			response = _result
+			response.status_code = 200
+			return response
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()	
+	
 
 if __name__ == "__main__":
     app.run()
