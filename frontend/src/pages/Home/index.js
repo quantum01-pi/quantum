f4e740f484767f6b756d9cd210165d1de097889a
Nick Oldfield <theparadoxnick@gmail.com>
2020-05-02 15:59:54 -0300

    Update de home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './styles.css'
+import '../../../node_modules/video-react/dist/video-react.css';
+import { Player, BigPlayButton } from 'video-react';
+
 import Header from '../../components/Header/index'
 import Footer from '../../components/Footer/index'
-import BannerHome from '../../assets/img/bannerHome.png'
+import BannerHome from '../../assets/img/bgHomeBlur.png'
 import Mudas from '../../assets/img/mudas.jpg'
 import Laranja from '../../assets/img/problema.png'
 import Celular from '../../assets/img/recursos.png'
 import Graos from '../../assets/img/graos.jpg'
 import Feira from '../../assets/img/feira.jpg'
+import Agro from '../../assets/img/agro.mp4'
 
-import Card from '../../components/Card'
-import Gallery from '../../components/Gallery'
 
 export default function Home() {
     return (
@@ -18,7 +20,21 @@ export default function Home() {
             <img src={BannerHome} className="bg-home" alt="Fundo borrado"/>
             <Header></Header>
             <div className="container">
-                <img src={BannerHome} className="banner" alt="Pessoa tocando plantação"/>
+                <div class="video">
+                    <div className="overlay">
+                        <div className="video__caption">
+                            <h2>Doar para gerar</h2>
+                            <p>A <strong>Quantum</strong> é uma <strong>plataforma web</strong> capaz fornecer um ponto de encontro entre empresas interessadas em <strong>doar alimentos</strong> e instituições que  tem o interesse em receber essas doações.</p>
+                            <p>As empresas doadoras podem utilizar dos benefícios fiscais devido suas doações.</p>
+                        </div>
+                    </div>
+                    <Player
+                        autoPlay
+                        loop
+                        src={Agro}>
+                        <BigPlayButton className="bigplaybutton" position="center" />
+                    </Player>
+                </div>
                 <section className="galeria">
                     <img src={Mudas} alt=""/>
                     <img src={Celular} alt=""/>
